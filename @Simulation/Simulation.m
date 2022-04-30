d27f2a2cdd1d6c9de40114b0dbc2c084e52ec2e6
Nathanael Gandhi <nat11public@gmail.com>
2022-04-30 17:15:35 +1000

    Add sim running loop

--- @Simulation/Simulation.m
+++ @Simulation/Simulation.m
@@ -2,7 +2,9 @@ classdef Simulation < handle
     % Vars
     properties
         simulation;     % Handle
-        logOb;
+        logOb;          % Log object
+        simRunning      % Flag for simulation desired state
+
     end
     % Const Vars
     properties(Constant)
@@ -32,6 +34,21 @@ classdef Simulation < handle
             surf([-4,-4;-4,-4],[-4,4;-4,4],[3,3;0,0],'CData',imread('assets/SideWall.jpg'),'FaceColor','texturemap');
             % Back Wall
             surf([-4,4;-4,4],[-4,-4;-4,-4],[3,3;0,0],'CData',imread('assets/BackWall.jpg'),'FaceColor','texturemap');
+        end
+        % Function to set the simulation running flag
+        function SetSimRunningFlag(self, flag)
+            self.simRunning = flag;
+        end
+        % Function to run simulation "main" loop
+        function RunSim(self)
+            while (self.simRunning)
+               % Sim running. Loop while flag condition is true 
+               if (self.simRunning)
+                   pause(1);
+                    self.logOb.LogDebug('[SIM] Sim Running');    
+               end
+            end
+             self.logOb.LogInfo('[SIM] Simulation Stopped');
         end
     end
 end
