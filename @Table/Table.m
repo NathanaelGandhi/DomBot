74657692e710904fea6afba25afb53846ac063fe
Nathanael Gandhi <nat11public@gmail.com>
2022-04-30 18:23:44 +1000

    Inherit from EnvironmentObject, construct superclass, apply pose

--- @Table/Table.m
+++ @Table/Table.m
@@ -1,29 +1,21 @@
-classdef Table < Log
+classdef Table < EnvironmentObject
     properties
-        model;  % Handle
-        logOb;  % Log object
-        
-        % Position of fire table
-        pose;
-        
         % Vertex count
         tableVertexCount;
     end
     
     methods%% Class for Table simulation
         function self = Table(logArg, pose)
-            self.logOb = logArg;    % Store log object
-            self.logOb.LogInfo('[TBL] Table constructor');
+            % Call superclass constructor
+            self = self@EnvironmentObject(logArg, pose, 'table');
             
             % I'm using this method as 'PlaceObject' can't rotate the table
             % Read ply file
-            [tris,verts,data] = plyread('Table.ply','tri');  
+            [tris,verts,data] = plyread('assets/Table.ply','tri');  
             % Set vertex count for use in transforms
             self.tableVertexCount = size(verts, 1);
             % Scale the colours to be 0-to-1 (they are originally 0-to-255
             vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
-            % Set desired base pose
-            self.pose = pose;
             % Update vertexes for new pose
             updatedPoints = [self.pose * [verts,ones(self.tableVertexCount,1)]']';
             % Then plot the trisurf with verticies
