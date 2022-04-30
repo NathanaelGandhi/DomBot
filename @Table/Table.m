fe185ea2b4a04b8f431641dfbd937918764de655
Nathanael Gandhi <nat11public@gmail.com>
2022-04-30 19:18:21 +1000

    Update asset location & environment object call

--- @Table/Table.m
+++ @Table/Table.m
@@ -11,7 +11,7 @@ classdef Table < EnvironmentObject
             
             % I'm using this method as 'PlaceObject' can't rotate the table
             % Read ply file
-            [tris,verts,data] = plyread('assets/Table.ply','tri');  
+            [tris,verts,data] = plyread('Table.ply','tri');  
             % Set vertex count for use in transforms
             self.tableVertexCount = size(verts, 1);
             % Scale the colours to be 0-to-1 (they are originally 0-to-255
@@ -19,7 +19,7 @@ classdef Table < EnvironmentObject
             % Update vertexes for new pose
             updatedPoints = [self.pose * [verts,ones(self.tableVertexCount,1)]']';
             % Then plot the trisurf with verticies
-            self.model = trisurf(tris,updatedPoints(:,1), updatedPoints(:,2), updatedPoints(:,3) ...
+            self.envObj = trisurf(tris,updatedPoints(:,1), updatedPoints(:,2), updatedPoints(:,3) ...
                 ,'FaceVertexCData',vertexColours,'EdgeColor','interp','EdgeLighting','flat');
         end
     end
