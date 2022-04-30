a04e8ddaa1bd577b5d2f11d018246d0f8d284a37
Nathanael Gandhi <nat11public@gmail.com>
2022-04-30 15:52:30 +1000

    Fix asset path to be called from app

--- @Simulation/Simulation.m
+++ @Simulation/Simulation.m
@@ -27,13 +27,13 @@ classdef Simulation < Log
             %   Detailed explanation goes here
             % Concrete Floor
             hold off
-            surf([-4,-4;4,4],[-4,4;-4,4],[0,0;0,0],'CData',imread('../assets/concrete.jpg'),'FaceColor','texturemap');
+            surf([-4,-4;4,4],[-4,4;-4,4],[0,0;0,0],'CData',imread('assets/concrete.jpg'),'FaceColor','texturemap');
             hold on
             % Side Walls - remove top for viewing
-            surf([4,4;4,4],[-4,4;-4,4],[3,3;0,0],'CData',imread('../assets/SideWall.jpg'),'FaceColor','texturemap');
-            surf([-4,-4;-4,-4],[-4,4;-4,4],[3,3;0,0],'CData',imread('../assets/SideWall.jpg'),'FaceColor','texturemap');
+            surf([4,4;4,4],[-4,4;-4,4],[3,3;0,0],'CData',imread('assets/SideWall.jpg'),'FaceColor','texturemap');
+            surf([-4,-4;-4,-4],[-4,4;-4,4],[3,3;0,0],'CData',imread('assets/SideWall.jpg'),'FaceColor','texturemap');
             % Back Wall
-            surf([-4,4;-4,4],[-4,-4;-4,-4],[3,3;0,0],'CData',imread('../assets/BackWall.jpg'),'FaceColor','texturemap');
+            surf([-4,4;-4,4],[-4,-4;-4,-4],[3,3;0,0],'CData',imread('assets/BackWall.jpg'),'FaceColor','texturemap');
         end
     end
 end
