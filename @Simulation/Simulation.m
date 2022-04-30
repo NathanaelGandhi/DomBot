c85ce29cc50e74d235c26e8a6757c2e8f27fab09
Nathanael Gandhi <nat11public@gmail.com>
2022-04-30 15:37:42 +1000

    Calling class function from app

--- @Simulation/Simulation.m
+++ @Simulation/Simulation.m
@@ -11,12 +11,29 @@ classdef Simulation < Log
     methods
         % Constructor
         function self = Simulation()
-            self@Log();     % Superclass Log
+%             self@Log();
             LogInfo(self,'[SIM] Simulation constructor');
+            %logOb.LogInfo(self,'[SIM]');
+            %GenerateEnvironment();
         end
         %Deconstructor
         function delete(self)
             LogInfo(self,'[SIM] Simulation deconstructor');
+        end
+        function GenerateEnvironment(self)
+            LogInfo(self,'[SIM] GenerateEnvironment()');
+            disp('[SIM] Disp GenerateEnvironment()');
+            %Generate Environment
+            %   Detailed explanation goes here
+            % Concrete Floor
+            hold off
+            surf([-4,-4;4,4],[-4,4;-4,4],[0,0;0,0],'CData',imread('../assets/concrete.jpg'),'FaceColor','texturemap');
+            hold on
+            % Side Walls - remove top for viewing
+            surf([4,4;4,4],[-4,4;-4,4],[3,3;0,0],'CData',imread('../assets/SideWall.jpg'),'FaceColor','texturemap');
+            surf([-4,-4;-4,-4],[-4,4;-4,4],[3,3;0,0],'CData',imread('../assets/SideWall.jpg'),'FaceColor','texturemap');
+            % Back Wall
+            surf([-4,4;-4,4],[-4,-4;-4,-4],[3,3;0,0],'CData',imread('../assets/BackWall.jpg'),'FaceColor','texturemap');
         end
     end
 end
