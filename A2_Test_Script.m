4053d90a9ac9dcbf644dca5a66c9ef7b191e69ff
KodaE <koda4739@gmail.com>
2022-05-08 04:56:59 +1000

    This test code now demonstrates the new MyCobot trajectory functions, which incorporate RMRC

--- A2_Test_Script.m
+++ A2_Test_Script.m
@@ -1,9 +1,27 @@
 clf
-dombot = myCobot;
 
+cobotLog = Log;
+tempPose = transl(0,0,0);
+dombot = MyCobot(cobotLog, 2, tempPose);
 
-t1 = transl(-0.05, -0.27, 0.1);
-pause(2);
-dombot.calculateTraj(t1,100);
-dombot.runTraj();
+nextPose = transl(0.2, 0.1, 0.1);
+% Calculates the trajectory (using RMRC)
+dombot.CalculateTraj(nextPose, 500);
 
+% Runs trajectory that was just calculated
+for i=1:500
+dombot.RunTraj(i);
+
+% You can even break out of the for loop and the DomBot will just stop at  
+% that position until its next trajectory is calculated
+
+% if (i > 243)
+%     break;
+% end
+end
+
+nextPose = transl(0.2, 0.1, 0);
+dombot.CalculateTraj(nextPose, 200);
+for i=1:200
+dombot.RunTraj(i);
+end
