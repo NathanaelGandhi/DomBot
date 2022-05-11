e6a25c109d783a3b3c4df615a2376db6b4033772
Nathanael Gandhi <nat11public@gmail.com>
2022-05-12 00:37:42 +1000

    Add params for teach panel

--- @MyCobot/teachCartesian.m
+++ @MyCobot/teachCartesian.m
@@ -1,7 +1,12 @@
-function [outputArg1,outputArg2] = teachCartesian(inputArg1,inputArg2)
-%TEACHCARTESIAN Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+function teachCartesian(robot)
+%TEACHCARTESIAN Jog the robot using Cartesian coordinates
+    %-------------------------------
+    % parameters for teach panel
+    bgcol = [103 233 98]/255;  % background color
+    height = 0.06;  % height of slider rows
+    %-------------------------------
+    
+    
+
 end
 
