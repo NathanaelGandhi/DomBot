94834645a4a6da87911b25712ca9c9fc18ef7203
Nathanael Gandhi <nat11public@gmail.com>
2022-05-12 02:15:54 +1000

    Move all properties to handles & add modifier for AX,AY,AZ

--- @MyCobot/teachCartesian.m
+++ @MyCobot/teachCartesian.m
@@ -2,50 +2,68 @@ function teachCartesian(robot)
 %TEACHCARTESIAN Jog the robot using Cartesian coordinates
     %-------------------------------
     % parameters for teach panel
-    bgcol = [103 233 98]/255;  % background color
-    height = 0.06;  % height of slider rows
+    handles.robot=robot;
+    handles.bgcol=[103 233 98]/255;  % background color
+    handles.height = 0.06;  % height of slider rows
+    % Slider properties
+    handles.sliderLabels = ['X','Y','Z','x','y','z'];
+    handles.sliderLimits = [... 
+        -handles.robot.radiusOfMotion, handles.robot.radiusOfMotion; ...
+        -handles.robot.radiusOfMotion, handles.robot.radiusOfMotion; ...
+        handles.robot.pose(3,4), handles.robot.pose(3,4)+0.412; ...         %0.411 appears to be max simulated reach
+        0, 2*pi; ...
+        0, 2*pi; ...
+        0, 2*pi;]; 
     %-------------------------------
-    handles = InstallThePanel(robot,bgcol);
-    handles = SetQlimToFinite(robot,handles);
-    handles = GetCurrentRobotState(robot,handles);
-    MakeSliders(robot,handles,bgcol,height);
+    handles = InstallThePanel(handles);
+    handles = SetQlimToFinite(handles);
+    handles = GetCurrentRobotState(handles);
+    MakeSliders(handles);
 end
 
-function MakeSliders(robot,handles,bgcol,height)
+function MakeSliders(handles)
     %---- sliders
-    % Slider properties
-    sliderLabels = ['x','y','z'];
-    sliderLimits = [... 
-        -robot.radiusOfMotion, robot.radiusOfMotion; ...
-        -robot.radiusOfMotion, robot.radiusOfMotion; ...
-        robot.pose(3,4), robot.pose(3,4)+0.412]; %0.411 appears to be max simulated reach
     % Create the sliders
-    n = size(sliderLabels,2);      % xyz
+    n = size(handles.sliderLabels,2);      % xyz
     for j=1:n
         % slider label
         uicontrol(handles.panel, 'Style', 'text', ...
             'Units', 'normalized', ...
-            'BackgroundColor', bgcol, ...
-            'Position', [0 height*(n-j+2) 0.15 height], ...
+            'BackgroundColor', handles.bgcol, ...
+            'Position', [0 handles.height*(n-j+2) 0.15 handles.height], ...
             'FontUnits', 'normalized', ...
             'FontSize', 0.5, ...
-            'String', sprintf(sliderLabels(:,j)));
+            'String', sprintf(handles.sliderLabels(:,j)));
         
         % slider itself
+        switch(j)
+            case {1,2,3}
+                modifier=j;
+                sliderType = 4;
+            case 4
+                modifier=1;
+                sliderType = 3;
+            case 5
+                modifier=2;
+                sliderType = 3;
+            case 6
+                modifier=3;
+                sliderType = 3;
+        end
         handles.slider(j) = uicontrol(handles.panel, 'Style', 'slider', ...
             'Units', 'normalized', ...
-            'Position', [0.15 height*(n-j+2) 0.65 height], ...
-            'Min', sliderLimits(j,1), ...
-            'Max', sliderLimits(j,2), ...
-            'Value', handles.T6(j,4), ...
+            'Position', [0.15 handles.height*(n-j+2) 0.65 handles.height], ...
+            'Min', handles.sliderLimits(j,1), ...
+            'Max', handles.sliderLimits(j,2), ...
+            'Value', handles.T6(modifier,sliderType), ...
             'Tag', sprintf('Slider%d', j));
         
         % text box showing slider value, also editable
         handles.edit(j) = uicontrol(handles.panel, 'Style', 'edit', ...
             'Units', 'normalized', ...
-            'Position', [0.80 height*(n-j+2)+.01 0.20 0.9*height], ...
-            'BackgroundColor', bgcol, ...
-            'String', num2str(handles.T6(j,4), 3), ...
+            'Position', [0.80 handles.height*(n-j+2)+.01 0.20 0.9*handles.height], ...
+            'BackgroundColor', handles.bgcol, ...
+            'String', num2str(handles.T6(modifier,sliderType), 3), ...
             'HorizontalAlignment', 'left', ...
             'FontUnits', 'normalized', ...
             'FontSize', 0.4, ...
@@ -53,11 +71,11 @@ function MakeSliders(robot,handles,bgcol,height)
     end
 end
 
-function handles = GetCurrentRobotState(robot, handles)
+function handles = GetCurrentRobotState(handles)
     %---- get the current robot state
     if isempty(handles.q)
         % check to see if there are any graphical robots of this name
-        rhandles = findobj('Tag', robot.model.name);
+        rhandles = findobj('Tag', handles.robot.model.name);
         % find the graphical element of this name
         if isempty(rhandles)
             error('RTB:teach:badarg', 'No graphical robot of this name found');
@@ -69,30 +87,30 @@ function handles = GetCurrentRobotState(robot, handles)
             handles.q = info.q;
         end
     else
-    robot.model.plot(handles.q);
+    handles.robot.model.plot(handles.q);
     end
-    handles.T6 = robot.model.fkine(handles.q);
+    handles.T6 = handles.robot.model.fkine(handles.q);
 end
 
-function handles = SetQlimToFinite(robot,handles)
+function handles = SetQlimToFinite(handles)
     % we need to have qlim set to finite values for a prismatic joint
-    qlim = robot.model.qlim;
+    qlim = handles.robot.model.qlim;
     if any(isinf(qlim))
         error('RTB:teach:badarg', 'Must define joint coordinate limits for prismatic axes, set qlim properties for prismatic Links');
     end
     handles.q = [];
 end
 
-function handles = InstallThePanel(robot,bgcol)
+function handles = InstallThePanel(handles)
     %---- install the panel at the side of the figure
     % find the right figure to put it in
-    c = findobj(gca, 'Tag', robot.model.name);      % check the current axes
+    c = findobj(gca, 'Tag', handles.robot.model.name);      % check the current axes
     if isempty(c)
         % doesn't exist in current axes, look wider
-        c = findobj(0, 'Tag', robot.model.name);    % check all figures
+        c = findobj(0, 'Tag', handles.robot.model.name);    % check all figures
         if isempty(c)
             % create robot in arbitrary pose
-            robot.model.plot( zeros(1, robot.model.n) );
+            handles.robot.model.plot( zeros(1, handles.robot.model.n) );
             ax = gca;
         else
             ax = get(c(1), 'Parent');               % get first axis holding the robot
@@ -112,11 +130,11 @@ function handles = InstallThePanel(robot,bgcol)
     % create the panel itself
     panel = uipanel(handles.fig, ...
         'Title', 'Teach Cartesian', ...
-        'BackGroundColor', bgcol,...
+        'BackGroundColor', handles.bgcol,...
         'Position', [0 0 .25 1]);
     set(panel, 'Units', 'pixels'); % stop automatic resizing
     handles.panel = panel;
     set(handles.fig, 'Units', 'pixels');
-    set(handles.fig, 'ResizeFcn', @(src,event) resize_callback(robot.model, handles));
+    set(handles.fig, 'ResizeFcn', @(src,event) resize_callback(handles.robot.model, handles));
 end
 
