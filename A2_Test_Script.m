32d6d5e6f6114eea4f29ea59b884926e76a5aeaf
KodaE <koda4739@gmail.com>
2022-05-13 18:17:47 +1000

    Demonstrates RunTraj that doesn't pass increments

--- A2_Test_Script.m
+++ A2_Test_Script.m
@@ -11,7 +11,7 @@ dombot.CalculateTraj(nextPose, 500);
 
 % Runs trajectory that was just calculated
 for i=1:500
-dombot.RunTraj(i);
+dombot.RunTraj();
 
 % You can even break out of the for loop and the DomBot will just stop at  
 % that position until its next trajectory is calculated
@@ -24,5 +24,5 @@ end
 nextPose = transl(0.2, 0.1, 0);
 dombot.CalculateTraj(nextPose, 200);
 for i=1:200
-dombot.RunTraj(i);
+dombot.RunTraj();
 end
